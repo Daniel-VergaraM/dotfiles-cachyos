2d8573b364eb57cc8c353e4cdcbe58da4ff1af57
Daniel-VergaraM <d.vergaram2@uniandes.edu.co>
2026-06-01 23:12:34 -0500

    Alias a fastfetch y keybindings para mover entre workspaces

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 alias cls='clear'
 alias y='yazi'
 alias lg='lazygit'
+alias ff='fastfetch'
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
